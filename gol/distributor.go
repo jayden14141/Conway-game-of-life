68d7339bb667fafbab5b7d88f2a66a87826d5d2d
jayden99141 <sd21099@bristol.ac.uk>
2022-10-28 16:56:30 +0100

    modified distributer.go

--- gol/distributor.go
+++ gol/distributor.go
@@ -9,17 +9,47 @@ type distributorChannels struct {
 	ioInput    <-chan uint8
 }
 
+// func calculateNextState(world [][]uint8) (result [][]uint8) {
+// 	return
+// }
+
+
+// func calculateAliveCells(world [][]uint8) (alive []util.Cell) {
+// 	return
+// }
+
+// Constructs 'world' array every time, reading bytes from io.go
+func constructWorld(b chan uint8) [][] uint8 {
+	byte := <- b
+	return
+}
+
+
+
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
 
+
 	// TODO: Create a 2D slice to store the world.
+	world := make([][]uint8, p.ImageHeight)
+	for i := range world {
+		world[i] = make([]uint8, p.ImageWidth)
+	}
 
 	turn := 0
-
+	aliveCells := make([]util.Cell, p.ImageHeight * p.ImageWidth)
 	// TODO: Execute all turns of the Game of Life.
+	for t:= 0; t < p.Turns; t++ {
+		world = calculateNextState(constructWorld(c.ioInput))
+		calculateAliveCells()
 
+	}
 	// TODO: Report the final state using FinalTurnCompleteEvent.
-
+	report := FinalTurnComplete {
+		CompletedTurns: p.Turns,
+		Alive : ~
+	}
+	c.event <- report
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
 	<-c.ioIdle
