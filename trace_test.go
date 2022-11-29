fdbca7fe75028e34d9443be86e7fc750bfbe3acc
jayden14141 <sd21099@bristol.ac.uk>
2022-11-29 21:27:24 +0000

    Pprof

--- trace_test.go
+++ trace_test.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"runtime/trace"
 	"testing"
+
 	"uk.ac.bris.cs/gameoflife/gol"
 	"uk.ac.bris.cs/gameoflife/util"
 )
@@ -11,19 +12,26 @@ import (
 // TestTrace is a special test to be used to generate traces - not a real test
 func TestTrace(t *testing.T) {
 	traceParams := gol.Params{
-		Turns:       10,
-		Threads:     4,
-		ImageWidth:  64,
-		ImageHeight: 64,
+		Turns:       100,
+		Threads:     8,
+		ImageWidth:  512,
+		ImageHeight: 512,
 	}
 	f, _ := os.Create("trace.out")
 	events := make(chan gol.Event)
 	err := trace.Start(f)
 	util.Check(err)
 	go gol.Run(traceParams, events, nil)
-	for range events {
+	complete := false
+	for !complete {
+		event := <-events
+		switch event.(type) {
+		case gol.FinalTurnComplete:
+			complete = true
+		}
 	}
 	trace.Stop()
 	err = f.Close()
 	util.Check(err)
+
 }
