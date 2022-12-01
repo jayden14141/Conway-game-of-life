ab353a44090074dc67eff90f0ce56bc2a774ed1d
jayden99141 <sd21099@bristol.ac.uk>
2022-12-01 05:11:52 +0000

    Out for submission

--- trace_test.go
+++ trace_test.go
@@ -12,26 +12,19 @@ import (
 // TestTrace is a special test to be used to generate traces - not a real test
 func TestTrace(t *testing.T) {
 	traceParams := gol.Params{
-		Turns:       100,
-		Threads:     8,
-		ImageWidth:  512,
-		ImageHeight: 512,
+		Turns:       10,
+		Threads:     4,
+		ImageWidth:  64,
+		ImageHeight: 64,
 	}
 	f, _ := os.Create("trace.out")
 	events := make(chan gol.Event)
 	err := trace.Start(f)
 	util.Check(err)
 	go gol.Run(traceParams, events, nil)
-	complete := false
-	for !complete {
-		event := <-events
-		switch event.(type) {
-		case gol.FinalTurnComplete:
-			complete = true
-		}
+	for range events {
 	}
 	trace.Stop()
 	err = f.Close()
 	util.Check(err)
-
 }
